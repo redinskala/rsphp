aae6d6712d6c9feaf57fe830306ed01e634f6566
redin <redin@redinskala.com>
2019-12-06 15:51:41 -0600

    Agregando tres.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 	echo "<h1>Bienvenidos a App I</h1>";
 	echo "<br>";
-	echo "<a href='dos.php'>Haz clieck para ir a dos.php</a>";
+	echo "<a href='dos.php'>Haz click para ir a dos.php</a>";
+	echo "<br>";
+	echo "<a href='dos.php'>Haz click para ir a tres.php</a>";
 ?>
---
+++ tres.php
@@ -0,0 +1,5 @@
+<?php
+	echo "<h1>Bienvenido a App III";
+	echo "<br>";
+	echo "<a href='index.php'>Haz click aquí para App I</a>";
+?>
