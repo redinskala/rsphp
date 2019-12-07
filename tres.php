4fd11185ffd7b788722986ddf30956721c30b90e
redin <redin@redinskala.com>
2019-12-06 23:37:09 -0600

    Agregando el código

--- tres.php
+++ tres.php
@@ -1,5 +1,5 @@
 <?php
 	echo "<h1>Bienvenido a App III</h1>";
 	echo "<br>";
-	echo "<a href='index.php'>Haz click aquí para App I</a>";
+	echo "<a href='index.php'>Haz click para ir a App I</a>";
 ?>
